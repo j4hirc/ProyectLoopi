9e74c1b8ef77769b33effd81397d0347ede9eb31
j4hirc <112521047+j4hirc@users.noreply.github.com>
2025-12-25 12:01:24 -0500

    Update Material.java

--- src/main/java/com/example/demo/models/entity/Material.java
+++ src/main/java/com/example/demo/models/entity/Material.java
@@ -14,7 +14,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "material")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"}, ignoreUnknown = true)
 public class Material implements Serializable {
 
 	
